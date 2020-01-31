6a94ddb08db79374b88152df5b2971cb1682e9fb
Karl P <kf.polintan@gmail.com>
2020-01-31 13:15:43 -0800

    routes started, created book/movie/home components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { HomeComponent } from './components/home/home.component';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  { path: 'home', component: HomeComponent },
+  { path: 'books', component: BookListComponent},
+  // { path: 'book-details', component: BookDetailsComponent}, -- what's the right syntax for the book ID? --
+  { path: 'movies', component: MovieListComponent },
+  { path: 'movie-details/:imdbID', component: MovieDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    BookDetailsComponent,
+    MovieListComponent,
+    MovieDetailsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
